b18c32906ec5f70a757b762980e7cf9444cff2b9
Patrick Hoydar <patrick@wallacegraphics.com>
2024-07-08 17:18:07 -0400

    trying to hit auth endpoint

---
+++ src/uspsAuth.ts
@@ -0,0 +1,48 @@
+// const config = {
+//   username: 'atlantamailing',
+//   password: 'W@llaceG01',
+//   grant_type: 'authorization',
+//   response_type: 'token',
+//   scope: 'user.info.ereg,iv1.apis',
+//   client_id: '687b8a36-db61-42f7-83f7-11c79bf7785e',
+// }
+
+// export const getAuthToken = async () => {
+//   await fetch('https://services.usps.com/oauth/authenticate', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     mode: 'no-cors',
+//     body: JSON.stringify(config),
+//   })
+//   console.log('Auth token received')
+// }
+
+const myHeaders = new Headers()
+myHeaders.append('Content-Type', 'application/json')
+myHeaders.append('Cookie', 'TLTSID=b005aa92d8581663910300e0ed96a2ca; NSC_tfswjdft-mc=ffffffff3b223ef445525d5f4f58455e445a4a42378b')
+
+const raw = JSON.stringify({
+  username: 'atlantamailing',
+  password: 'W@llaceG01',
+  grant_type: 'authorization',
+  response_type: 'token',
+  scope: 'user.info.ereg,iv1.apis',
+  client_id: '687b8a36-db61-42f7-83f7-11c79bf7785e',
+})
+
+const requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  mode: 'no-cors',
+  redirect: 'follow',
+}
+
+export const getAuthToken = async () => {
+  await fetch('https://services.usps.com/oauth/authenticate', requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error))
+}
